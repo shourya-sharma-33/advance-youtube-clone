4d8f2d305847d49f748b462d213286cfee670aa6
shourya-sharma-33 <shourya-ml-dl-artificial-intelligence@proton.me>
2025-11-29 15:38:49 +0530

    TRPC SETUP : Added some moah stuff and configurations now rate limier lagana hai

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,23 +1,59 @@
-import { initTRPC } from '@trpc/server';
+import { db } from '@/db';
+import { users } from '@/db/schema';
+import { auth } from '@clerk/nextjs/server';
+import { initTRPC, TRPCError } from '@trpc/server';
 import { cache } from 'react';
 import superjson from 'superjson';
+import { eq } from "drizzle-orm";
+
+/**
+ * Single, correct TRPC context
+ */
 export const createTRPCContext = cache(async () => {
-    /**
-     * @see: https://trpc.io/docs/server/context
-     */
-    return { userId: 'user_123' };
+    const { userId } = await auth();
+
+    return {
+        clerkUserId: userId,
+    };
 });
-// Avoid exporting the entire t-object
-// since it's not very descriptive.
-// For instance, the use of a t variable
-// is common in i18n libraries.
-const t = initTRPC.create({
-    /**
-     * @see https://trpc.io/docs/server/data-transformers
-     */
+
+export type Context = Awaited<ReturnType<typeof createTRPCContext>>;
+
+/**
+ * Init TRPC
+ */
+const t = initTRPC.context<Context>().create({
     transformer: superjson,
 });
-// Base router and procedure helpers
+
+/**
+ * Exports
+ */
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(async function isAuthed(opts) {
+    const { ctx } = opts;
+    if (!ctx.clerkUserId) {
+        throw new TRPCError(
+            { code: "UNAUTHORIZED" }
+        )
+    }
+
+    const [user] = await db
+        .select()
+        .from(users)
+        .where(eq(users.clerkId, ctx.clerkUserId))
+        .limit(1);
+
+    if (!user) {
+        throw new TRPCError({code : "UNAUTHORIZED"})
+    }
+
+    return opts.next({
+        ctx: {
+            ...ctx,
+            user
+        }
+    })
+})
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,18 +1,19 @@
 import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { createTRPCRouter, protectedProcedure } from '../init';
 import { auth } from '@clerk/nextjs/server';
 export const appRouter = createTRPCRouter({
-    hello: baseProcedure
+    hello: protectedProcedure
         .input(
             z.object({
                 text: z.string(),
             }),
         )
-        .query(async (opts) => {
-            const {userId} = await auth();
-            console.log({userId})
+        .query(async (opts) => { 
+            
+            console.log({fromContext : opts.ctx.user})
             return {
                 greeting: `hello ${opts.input.text}`,
+                
             };
         }),
 });
