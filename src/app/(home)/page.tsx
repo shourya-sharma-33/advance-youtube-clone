1ee44f10dfb521f1e7b3482aed7d1d290633da44
shourya-sharma-33 <shourya-ml-dl-artificial-intelligence@proton.me>
2025-11-29 14:25:22 +0530

    TRPC issue Fixed : Final Done

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,21 @@
-
-import { trpc } from "@/trpc/server";
+import { HydrateClient, trpc } from "@/trpc/server";
 import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
 export default async function Home() {
-  trpc.hello.prefetch({
+
+  await trpc.hello.prefetch({
     text: "meow"
   });
-  return <PageClient />;
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>loading..</p>}>
+        <ErrorBoundary fallback={<p>Error..</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
